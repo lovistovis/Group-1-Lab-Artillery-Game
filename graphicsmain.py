0fc6bd99ff0fdba2f69443a2977debf266b55610
Love Lysell Berglund <lovistovis0@gmail.com>
2025-10-02 13:59:04 +0200

    remove comment

--- graphicsmain.py
+++ graphicsmain.py
@@ -14,9 +14,6 @@ class GameGraphics:
         self.win = GraphWin("Cannon game" , 640, 480, autoflush=False)
         self.win.setCoords(-110, -10, 110, 155)
 
-        # draw the terrain
-        # TODO: Draw a line from (-110,0) to (110,0)
-        
         Line(Point(-110,0),Point(110,0)).draw(self.win)
 
         self.draw_cannons = [self.drawCanon(0), self.drawCanon(1)]
